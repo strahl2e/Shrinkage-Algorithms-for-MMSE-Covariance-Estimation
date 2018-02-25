ad0a26f19015043003d56f3a8c510a46cd16b416
JStrahl <jonathan.strahl@aalto.fi>
2018-02-25 23:33:23 +0200

    Add estimate for true covariance computing rho with the known covariance as a lower bound on the MSE.

--- MSE_simulations.m
+++ MSE_simulations.m
@@ -58,10 +58,12 @@ ns = 6:2:30;
 avg_rho_LWs = zeros(length(tst_cov_mats), length(ns),1);
 avg_rho_RBLWs = zeros(length(tst_cov_mats), length(ns),1);
 avg_rho_OASs = zeros(length(tst_cov_mats), length(ns),1);
+avg_rho_Ora = zeros(length(tst_cov_mats), length(ns),1);
 
 avg_MSE_LWs = zeros(length(tst_cov_mats), length(ns), 1);
 avg_MSE_RBLWs = zeros(length(tst_cov_mats), length(ns), 1);
 avg_MSE_OASs = zeros(length(tst_cov_mats), length(ns), 1);
+avg_MSE_Ora = zeros(length(tst_cov_mats), length(ns), 1);
 
 for tst_mat_i = 1:length(tst_cov_mats)
     for n = ns
@@ -69,14 +71,17 @@ for tst_mat_i = 1:length(tst_cov_mats)
         sum_rhos_LW = 0;
         sum_rhos_RBLW = 0;
         sum_rhos_OAS = 0;
+        sum_rhos_Ora = 0;
 
         sum_MSEs_LW = 0;
         sum_MSEs_RBLW = 0;
         sum_MSEs_OAS = 0;
+        sum_MSEs_Ora = 0;
 
         for i = 1:iters
             % Create n samples using chosen covariance matrix
-            X = mvnrnd(zeros(p,1), tst_cov_mats{tst_mat_i}, n);
+            TrueCov = tst_cov_mats{tst_mat_i};
+            X = mvnrnd(zeros(p,1), TrueCov, n);
 
             % Approximate covariance matrix using LW, RBLW, OAS and true oracle
             S_hat = cov(X,1);
@@ -94,45 +99,48 @@ for tst_mat_i = 1:length(tst_cov_mats)
             OAS_numer = trace(S_hat^2) + ((1-2)/p)*trace(S_hat)^2;
             OAS_denom = ((n+1-2)/p)*(trace(S_hat^2) - ((trace(S_hat)^2)/p));
             rho_OAS = min(1, OAS_numer / OAS_denom);
-
+            
+            Ora_numer = (1 - 2/p)*trace(TrueCov^2) + trace(TrueCov)^2;
+            Ora_denom = (n + 1 - 2/p)*trace(TrueCov^2) + (1 - n/p)*trace(TrueCov)^2;
+            rho_Ora = Ora_numer / Ora_denom;
+            
             S_hat_LW = (1-rho_LW)*S_hat + rho_LW * F_hat;
             S_hat_RBLW = (1-rho_RBLW)*S_hat + rho_RBLW * F_hat;
             S_hat_OAS = (1-rho_OAS)*S_hat + rho_OAS * F_hat;
+            S_hat_Ora = (1-rho_Ora)*S_hat + rho_Ora * F_hat;
 
-            % Record MSE
-            LW_Fro_sq_sum = 0;
-            RBLW_Fro_sq_sum = 0;
-            OAS_Fro_sq_sum = 0;
-            for j=1:n
-                LW_Fro_sq_sum = LW_Fro_sq_sum + norm(S_hat_LW - S_AR1_r_0_1, 'fro')^2;
-                RBLW_Fro_sq_sum = RBLW_Fro_sq_sum + norm(S_hat_RBLW - S_AR1_r_0_1, 'fro')^2;
-                OAS_Fro_sq_sum = OAS_Fro_sq_sum + norm(S_hat_OAS - S_AR1_r_0_1, 'fro')^2;
-            end
-            sum_MSEs_LW = LW_Fro_sq_sum / n;
-            sum_MSEs_RBLW = RBLW_Fro_sq_sum / n;
-            sum_MSEs_OAS = OAS_Fro_sq_sum / n;
+            % Record MSEs
+            sum_MSEs_LW = sum_MSEs_LW + norm(S_hat_LW - S_AR1_r_0_1, 'fro')^2;
+            sum_MSEs_RBLW = sum_MSEs_RBLW + norm(S_hat_RBLW - S_AR1_r_0_1, 'fro')^2;
+            sum_MSEs_OAS = sum_MSEs_OAS + norm(S_hat_OAS - S_AR1_r_0_1, 'fro')^2;
+            sum_MSEs_Ora = sum_MSEs_Ora + norm(S_hat_Ora - S_AR1_r_0_1, 'fro')^2;
 
             % Track rho values
             sum_rhos_LW = sum_rhos_LW + rho_LW;
             sum_rhos_RBLW = sum_rhos_RBLW + rho_RBLW;
             sum_rhos_OAS = sum_rhos_OAS + rho_OAS;
+            sum_rhos_Ora = sum_rhos_Ora + rho_Ora;
         end % Repeat 'iter' times
         % Record average rho values and MSEs
         i_n = find(ns == n);
         avg_rho_LWs(tst_mat_i,i_n,:) = sum_rhos_LW / iters;
         avg_rho_RBLWs(tst_mat_i,i_n,:) = sum_rhos_RBLW / iters;
         avg_rho_OASs(tst_mat_i,i_n,:) = sum_rhos_OAS / iters;
+        avg_rho_Ora(tst_mat_i,i_n,:) = sum_rhos_Ora / iters;
 
         avg_MSE_LWs(tst_mat_i,i_n,:) = sum_MSEs_LW / iters;
         avg_MSE_RBLWs(tst_mat_i,i_n,:) = sum_MSEs_RBLW / iters;
         avg_MSE_OASs(tst_mat_i,i_n,:) = sum_MSEs_OAS / iters;
+        avg_MSE_Ora(tst_mat_i,i_n,:) = sum_MSEs_Ora / iters;
     end
     figure
     hold on
     plot(avg_rho_LWs(tst_mat_i,:,:))
     plot(avg_rho_RBLWs(tst_mat_i,:,:))
     plot(avg_rho_OASs(tst_mat_i,:,:))
+    plot(avg_rho_Ora(tst_mat_i,:,:))
     title(['Covariance ', num2str(tst_mat_i)])
+    legend('LW', 'RBLW', 'OAS', 'Oracle');
     hold off
 end
 
