8e095472001a8d6436a63dc23ec82316d3b6fc35
JStrahl <jonathan.strahl@aalto.fi>
2018-03-03 13:35:21 +0200

    Real-world example using Shrinkage for covariance estimation.

---
+++ PortfolioManShrinkCovEst.m
@@ -0,0 +1,8 @@
+%% Portfolio management with covariance shrinkage estimators
+
+% 1. Read in stock data for large n, large p
+
+
+% 2. Try wealth management for small n, increasing n, comparing use of
+% sample covariance vs methods in paper
+
