190ccc6e05200afdeae72fd4aba49b2510d3300b
JStrahl <jonathan.strahl@aalto.fi>
2018-03-06 14:27:06 +0200

    As the portfolio management comparison was unclear as to how well the true covariance was approximated, this is a more direct approach.

---
+++ PortfolioManagementCovEstDirectComparison.m
@@ -0,0 +1,7 @@
+%% Compare covariance approximation of a number of stocks
+
+%% Read in stocks
+
+%% Use all samples as a 'true' covariance
+
+%% Now approximate the 'true' covariance  with subsamples and compare methods
